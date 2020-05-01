6b3e43cef0635a23dedd1d6fbb7c201795450ea8
Monika Toth <0monika01@gmail.com>
2020-05-01 14:00:59 +0200

    Updated: filerw to create the file if it does not exist.

--- models/FileRW.py
+++ models/FileRW.py
@@ -4,24 +4,24 @@ class FileRW:
         self.bst = bst
 
     def read_insert_to_bst(self):
-        with open(self.filename, "r") as to_read:
+        with open(self.filename, "r+") as to_read:
             for line in to_read:
                 if line != "\n":
                     self.bst.insert(line, True)
 
     def write(self, event):
-        with open(self.filename, "r") as f:
+        with open(self.filename, "r+") as f:
             last_line = f.readline(-1)
-        with open(self.filename, "a") as f:
+        with open(self.filename, "a+") as f:
             if last_line == "\n":
                 f.write(event)
             else:
                 f.write("\n"+event)
 
     def remove(self, event):
-        with open(self.filename, "r") as f:
+        with open(self.filename, "r+") as f:
             lines = f.readlines()
-        with open(self.filename, "w") as f:
+        with open(self.filename, "w+") as f:
             for line in lines:
                 if line.strip("\n") != event and line.strip("\n") != "":
                     f.write(line)
